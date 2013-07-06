3b72b870189aeb5d6a8ed85412e8d8ad958ed1bd
chetan.bhatt <bhatt@adobe.com>
2013-07-07 01:37:56 +0530

    Added client core project

---
+++ admo-client-core/src/main/java/com/admo/App.java
@@ -0,0 +1,13 @@
+package com.admo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
